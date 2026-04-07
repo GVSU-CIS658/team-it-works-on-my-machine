ac15510ab8e1032bc97804f41d96076beb2faf4f
Lloyd Git Nguyen <LloydNguyen3d@gmail.com>
2026-04-07 01:34:11 +0000

    create firebase

---
+++ firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAunnND1hshsiwwXqJAJ0d9a5oB9ryDY6s",
+    authDomain: "finalproject-2cac4.firebaseapp.com",
+    projectId: "finalproject-2cac4",
+    storageBucket: "finalproject-2cac4.firebasestorage.app",
+    messagingSenderId: "128135548975",
+    appId: "1:128135548975:web:2f4f0f11d765e5bba176da"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//to have firebase working
+
+//npm install firebase
+
+//npm install -g firebase-tools
