a9b895376fa55ba582215d3c755eac2380304501
Lloyd Git Nguyen <LloydNguyen3d@gmail.com>
2026-04-07 23:12:57 +0000

    Add firebase into project

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <script setup>
 import { computed } from 'vue'
 import { useRoute } from 'vue-router'
+import  db from '../firebase.ts'
+import {doc, collection, DocumentSnapshot, getDoc} from "firebase/firestore"
 
 import AuthLayout from './components/AuthLayout.vue'
 import SiteLayout from './components/SiteLayout.vue'
 
-const route = useRoute()
-const activeLayout = computed(() => route.meta.layout ?? 'site')
+const route = useRoute();
+const activeLayout = computed(() => route.meta.layout ?? 'site');
+
+function testingData(){
+const testData = doc(db, "test/testing");
+getDoc(testData).then((ds) => {
+if(ds.exists()){
+  console.log(ds.data());
+}
+else{console.log("data doesn't exist");}
+});}
+
+//testingData();
+
 </script>
 
 <template>
